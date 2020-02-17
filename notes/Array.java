78c1a51e67d3ba4998b9e2afe4242f6961b012f8
lighteningzhang <lighteningzhang@163.com>
2020-02-17 16:43:55 +0800

    Study JAVA Array

---
+++ notes/Array.java
@@ -0,0 +1,12 @@
+class Array{
+    public static void main(String[] args) {
+        // int[] y = null;
+        // y = new int[]{1,2,3};
+        // System.out.println(y[2]);
+        int [][] pT;
+        pT = new int[4][];
+        pT[0] = new int[]{1,2};
+        pT[1] = new int[]{5,7};
+        int[] rowZero = pT[0];
+    }
+}
